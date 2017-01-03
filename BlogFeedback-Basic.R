b2c9ca27c2fd8d2f282e561728303617cbe900f9
Francis Hsu <Francis-Hsu@users.noreply.github.com>
2017-01-02 18:03:56 -0600

    Add files via upload

--- BlogFeedback-Basic.R
+++ BlogFeedback-Basic.R
@@ -1,7 +1,9 @@
-### Simple Models ###
+### Basic Models ###
 library(data.table)
 library(MatrixModels)
 
+library(e1071)
+library(FNN)
 library(glmnet)
 library(ranger)
 library(xgboost)
@@ -19,6 +21,7 @@ mse = function(y_hat, y) {
 
 # create design matrices
 train_x = model.Matrix(V281 ~ . - 1, data = train, sparse = F)
+train_x_sparse = model.Matrix(V281 ~ . - 1, data = train, sparse = T)
 train_y = train$V281
 
 test_x = model.Matrix(V281 ~ . - 1, data = test, sparse = F)
@@ -27,15 +30,24 @@ test_y = test$V281
 train_xgb = xgb.DMatrix(data = as.matrix(train_x), label = train_y)
 test_xgb = xgb.DMatrix(data = as.matrix(test_x), label = test_y)
 
+# try kNN
+pred_knn = knn.reg(train_x, test_x, train_y, k = 19)$pred
+mse(pred_knn, test_y)
+
 # try LASSO
-mdl_lasso = cv.glmnet(train_x, train_y, family = "gaussian", alpha = 1)
+mdl_lasso = cv.glmnet(train_x_sparse, train_y, family = "gaussian", alpha = 1)
 pred_lasso = predict(mdl_lasso, newx = test_x)
 mse(pred_lasso, test_y)
 
 # try random forest
-mdl_rf = ranger(V281 ~ ., data = train, num.trees = 500, mtry = 120, write.forest = T)
+mdl_rf = ranger(V281 ~ ., data = train, num.trees = 1000, mtry = 120, write.forest = T)
 pred_rf = predict(mdl_rf, test)
 mse(pred_rf$predictions, test_y)
+
+# try SVM
+mdl_svm = svm(V281 ~ V52 + V55 + V61 + V51 + V54 + V21 + V6 + V10, data = train, kernel = "radial", cost = 2, gamma = 0.25)
+pred_svm = predict(mdl_svm, test)
+mse(pred_svm, test_y)
 
 # try XGboost
 mdl_xgb = xgboost(data = train_xgb, nround = 750, nthread = 4, max_depth = 6, eta = 0.025, subsample = 0.7, gamma = 3)
